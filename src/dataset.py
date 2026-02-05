3fb2488eca3c8e109cbc24fdda6d8bfd0c373cc1
Iwamoto Melo Bernardo <iwamob00@jcu.cz>
2026-02-05 16:29:39 +0100

    src/dataset funcionando. 27300 arquivos. csv com caminho da imagem, mascara, nome do arq e folder

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-base_path = Path("../data")
+script_dir = Path(__file__).parent
+base_path = script_dir.parent / "data"
 
 img_folders = ['GID-img-1/1', 'GID-img-2/2', 'GID-img-3/3', 'GID-img-4/4']
 label_folder = 'GID-label'
@@ -14,7 +15,6 @@ for folder in img_folders:
 
     if img_dir.exists():
         for img_file in img_dir.iterdir():
-
             if img_file.is_file() and img_file.suffix.lower() == '.jpg':
                 label_name = img_file.stem + ".png"
                 label_file = label_dir / label_name
@@ -31,5 +31,7 @@ for folder in img_folders:
 df = pd.DataFrame(data_list)
 
 df.to_csv("dataset_map.csv")
+
+df.head()  
 
 print(f"dataframe criado: {len(df)} pares")
